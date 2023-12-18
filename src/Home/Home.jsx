0eea59febf860e1c140c2f22498ff9a986d4627e
LouisWorld-HPT <102909727+LouisWorld-HPT@users.noreply.github.com>
2023-12-19 00:25:07 +0630

    First Commit

--- src/Home/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import HomeComponentOne from "./HomeComponentOne";
-export default function Home(){
-    return(
-        <HomeComponentOne/>          
-    )
-}
--- src/posts/PostRenderPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function PostRenderPage(){
-    return (
-        <h1>Post Render Page</h1>
-    )
-}
